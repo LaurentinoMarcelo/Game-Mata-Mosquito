6dd34a0567d13e607a117cc701d107b292ee1a3b
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-09-11 11:14:12 -0300

    Finalizando jogo mata mosquito
    Finalizado a configuração sistema de vida e cronometro, juntamente com a criação da tela de vitória e game over.

--- jogo.js
+++ jogo.js
@@ -1,6 +1,20 @@
 
 var altura = 0;
-var largura = 0; 
+var largura = 0;
+var vidas = 1; 
+var tempo = 10;
+var criaMosquitoTempo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace('?', '');
+
+if (nivel === 'normal') {
+    criaMosquitoTempo = 1500;
+}else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000;
+}else if(nivel === 'chucknorris'){
+    criaMosquitoTempo = 750;
+}
 
 function ajusteTamanhoTela(){
 
@@ -10,11 +24,31 @@ function ajusteTamanhoTela(){
 
 ajusteTamanhoTela();
 
+var cronometro = setInterval(function(){
+    tempo -=1
+    if (tempo < 0) {
+        clearInterval(cronometro);
+        clearInterval(criaMosquito);
+        window.location.href = 'vitoria.html'
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo;
+    }
+    
+},1000);
+
 function posicaoRandomica(){
 
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove();
+
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        }else{
+        document.getElementById('v' + vidas).src="imagens/coracao_vazio.png";
+        vidas++;
+        }
     }
+
     
 
     var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -29,7 +63,10 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
-    mosquito.id = 'mosquito'
+    mosquito.id = 'mosquito';
+    mosquito.onclick = function(){
+        this.remove();
+    }
 
     document.body.appendChild(mosquito)
 }
